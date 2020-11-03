b9a7c3eb9a8eac71a0ff10d468cb24469317e835
viniciusmilanez <vinicius.254711@alunosatc.edu.br>
2020-11-03 14:20:31 -0300

    Finalizando calculadora

--- App.js
+++ App.js
@@ -1,21 +1,169 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import InputNumberButton from './InputNumberButton';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const buttons = [
+  ['CLEAR', 'EXCLUIR'],
+  ['7', '8', '9', '%'],
+  ['4', '5', '6', 'x'],
+  ['1', '2', '3', '-'],
+  ['0', '.', '=', '+'],
+]
+export default class App extends Component {
+
+  constructor(){
+    super()
+    this.initialState = {
+      displayValue: '0',
+      operator: null,
+      firstValue: '',
+      secondValue: '',
+      nextValue: false
+    }
+    this.state = this.initialState;
+  }
+  renderButtons() {
+    let layouts = buttons.map((buttonRows, index) => {
+      let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
+        return <InputNumberButton
+          value={buttonItems}
+          handleOnPress={this.handleInput.bind(this, buttonItems)}
+          key={'btn-' + buttonIndex}
+        />
+      });
+
+      return <View style={styles.inputRow} key={'row-' + index}>{rowItem}</View>
+    });
+
+    return layouts
+  }
+
+  handleInput = (input) => {
+    const { displayValue, operator, firstValue, secondValue, nextValue } = this.state;
+    switch (input) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9': 
+        this.setState({
+          displayValue: (displayValue === '0') ? input : displayValue + input
+        })
+
+        if(!nextValue){
+          this.setState({
+            firstValue: firstValue + input
+          })
+        } else {
+          this.setState({
+            secondValue: secondValue + input
+          })
+        }
+        
+        break;
+      case '+':
+      case '-':
+      case 'x':
+      case '%':
+        this.setState({
+          nextValue: true,
+          operator: input,
+          displayValue: (operator !== null ? displayValue.substr(0, displayValue.length - 1) : displayValue) + input
+        })
+        break;
+
+      case '.':
+        let dot = displayValue.toString().slice(-1) 
+        this.setState({
+          displayValue: dot !== '.' ? displayValue + input : displayValue
+        })
+
+        if(!nextValue){
+          this.setState({
+            firstValue: firstValue + input
+          })
+        } else {
+          this.setState({
+            secondValue: secondValue + input
+          })
+        }
+        break;
+
+      case '=':
+        let formatOperator = (operator == 'x') ? '*' : (operator == '%') ? '/' : operator
+        let result = eval(firstValue + formatOperator + secondValue)
+        this.setState({
+          displayValue: result % 1 === 0 ? result : result.toFixed(2),
+          firstValue: result % 1 === 0 ? result : result.toFixed(2),
+          secondValue: '',
+          operator: null,
+          nextValue: false
+        })
+        break;
+
+      case 'CLEAR':
+        this.setState(this.initialState);
+        break;
+
+      case 'EXCLUIR':
+        let string = displayValue.toString();
+        let deletedString = string.substr(0, string.length - 1);
+        let length = string.length;
+        this.setState({
+          displayValue: length == 1 ? '0' : deletedString,
+          firstValue: length == 1 ? '' : deletedString
+        })
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultText}>
+            {this.state.displayValue}
+          </Text>
+        </View>
+
+        <View style={styles.inputContainer}>
+          {this.renderButtons()}
+        </View>
+
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  resultContainer: {
+    flex: 2,
+    justifyContent: 'center',
+    backgroundColor: '#1a3138',
+  },
+  inputContainer: {
+    flex: 8,
+    backgroundColor: '#00c7ff',
+  },
+  resultText: {
+    color: 'lightgray',
+    fontSize: 40,
+    fontWeight: 'bold',
+    padding: 20,
+    textAlign: 'right',
+  },
+  inputRow: {
+    flex: 1,
+    flexDirection: 'row'
+  }
 });
---
+++ InputNumberButton.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default class InputNumberButton extends Component {
+    render(){
+        const {value, handleOnPress} = this.props;
+
+        return (
+            <TouchableOpacity style={styles.container} onPress={()=> handleOnPress(value)}>
+                <Text style={styles.text}>{value}</Text>
+            </TouchableOpacity>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      margin: 1,
+      backgroundColor: 'rgba(255, 255, 255, 0.1)',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    text: {
+        color: '#ee12c1',
+        fontSize: 26,
+    },
+  });
